96ba48929ce79ae3f134cc53b6ed8024b657bdf3
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-02-28 22:07:08 +0700

    done : praktikum 3 perobaan 3

--- Semester2/Pertemuan3/ArrayBalok/Segitiga.java
+++ Semester2/Pertemuan3/ArrayBalok/Segitiga.java
@@ -8,4 +8,13 @@ public class Segitiga {
         alas = a;
         tinggi = t;
     }
+
+    public double hitungLuas() {
+        return (double) (alas * tinggi) / 2;
+    }
+
+    public double hitungKeliling() {
+        double sisiMiring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+        return (double) alas + tinggi + sisiMiring;
+    }
 }
