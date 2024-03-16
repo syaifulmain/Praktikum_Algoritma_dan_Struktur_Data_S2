fe47569ef14a8b1f3c950f018acf2d8e2ee9eb88
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-03-16 14:48:25 +0700

    percobaan 3 pertanyaan ke 3 menghitung keuntungan lebih dari perusahaan dengan jumlah bulan yang berbeda

--- src/main/java/pertemuan5/percobaan3/MainSum.java
+++ src/main/java/pertemuan5/percobaan3/MainSum.java
@@ -1,30 +1,82 @@
 package pertemuan5.percobaan3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("============================================================");
-        System.out.println("Program Menghitung Keuntungan Total (Satuan Juta. Misal 5.9)");
-        System.out.print("Masukan jumlah bulan : ");
-        int element = scanner.nextInt();
-        Sum sum = new Sum(element);
-        System.out.println("============================================================");
+        printTitle();
+        Perusahaan[] perusahaan = jumlahPerusahaan(scanner);
+        isiPerusahaan(perusahaan, scanner);
+        printPerusahaan(perusahaan);
+        keuntunganSeluruhPerusahaan(perusahaan);
+    }
+    static Perusahaan[] jumlahPerusahaan(Scanner scanner) {
+        System.out.print("Masukkan jumlah perusahaan : ");
+        return new Perusahaan[scanner.nextInt()];
+    }
+
+    static void isiPerusahaan(Perusahaan[] perusahaan, Scanner scanner) {
+        for (int i = 0; i < perusahaan.length; i++) {
+            System.out.println("Perusahaan ke-" + (i + 1));
+            System.out.print("Masukan nama perusahaan : ");
+            String namaPerusahaan = scanner.next();
+            System.out.print("Masukan jumlah bulan : ");
+            int jumlahBulan = scanner.nextInt();
+            perusahaan[i] = new Perusahaan(namaPerusahaan, jumlahBulan, new Sum(jumlahBulan));
+            isiKeuntungan(perusahaan[i].sum, scanner);
+            System.out.println("============================================================");
+        }
+    }
+
+    static void printPerusahaan(Perusahaan[] perusahaan) {
+        for (int i = 0; i < perusahaan.length; i++) {
+            System.out.println("============================================================");
+            System.out.println("Perusahaan ke-" + (i + 1));
+            System.out.println("Nama Perusahaan : " + perusahaan[i].namaPerusahaan);
+            System.out.println("Jumlah Bulan : " + perusahaan[i].jumlahBulan);
+            System.out.println("============================================================");
+            sumBF(perusahaan[i].sum);
+            System.out.println("============================================================");
+            sumDC(perusahaan[i].sum);
+            System.out.println("============================================================");
+        }
+    }
+
+    static void isiKeuntungan(Sum sum, Scanner scanner) {
         for (int i = 0; i < sum.element; i++) {
             System.out.print("Masukan untung bulan ke-" + (i + 1) + " = ");
             sum.keuntungan[i] = scanner.nextDouble();
         }
-        System.out.println("============================================================");
+    }
+
+    static void sumBF(Sum sum) {
         System.out.println("Algoritma Brute Force");
+        sum.total = sum.totalBF(sum.keuntungan);
         System.out.printf("Total keuntungan perusahaan selama %d bulan adalah %.2f juta%n",
                 sum.element,
-                sum.totalBF(sum.keuntungan));
-        System.out.println("============================================================");
+                sum.total);
+    }
+
+    static void sumDC(Sum sum) {
         System.out.println("Algoritma Divide and Conquer");
+        sum.total = sum.totalDC(sum.keuntungan, 0, sum.element - 1);
         System.out.printf("Total keuntungan perusahaan selama %d bulan adalah %.2f juta%n",
                 sum.element,
-                sum.totalDC(sum.keuntungan, 0, sum.element - 1));
+                sum.total);
+    }
+
+    static void printTitle() {
+        System.out.println("============================================================");
+        System.out.println("Program Menghitung Keuntungan Total (Satuan Juta. Misal 5.9)");
+        System.out.println("============================================================");
+    }
+
+    static void keuntunganSeluruhPerusahaan(Perusahaan[] perusahaan) {
+        double total = Arrays.stream(perusahaan).mapToDouble(p -> p.sum.total).sum();
+        System.out.println("============================================================");
+        System.out.printf("Total keuntungan dari %s perusahaan adalah %.2f juta%n", perusahaan.length, total);
         System.out.println("============================================================");
     }
 }
---
+++ src/main/java/pertemuan5/percobaan3/Perusahaan.java
@@ -0,0 +1,13 @@
+package pertemuan5.percobaan3;
+
+public class Perusahaan {
+    String namaPerusahaan;
+    int jumlahBulan;
+    Sum sum;
+
+    public Perusahaan(String namaPerusahaan, int jumlahBulan, Sum sum) {
+        this.namaPerusahaan = namaPerusahaan;
+        this.jumlahBulan = jumlahBulan;
+        this.sum = sum;
+    }
+}
