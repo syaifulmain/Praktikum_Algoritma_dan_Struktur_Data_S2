6b1c79958bb9ddf2297dcd1189fa59f35dde86ce
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-03-21 10:36:13 +0700

    add percobaan 3

---
+++ src/main/java/pertemuan6/percobaan3/DaftarMahasiswaBerprestasi.java
@@ -0,0 +1,33 @@
+package pertemuan6.percobaan3;
+
+public class DaftarMahasiswaBerprestasi {
+    private Mahasiswa[] listMhs = new Mahasiswa[5];
+    private int idx;
+    void tambahMahasiswa(Mahasiswa mhs) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = mhs;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa mhs : listMhs) {
+            mhs.tampil();
+            System.out.println("=====================================");
+        }
+    }
+
+    void insertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].getIpk() > temp.getIpk()) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
+        }
+    }
+}
---
+++ src/main/java/pertemuan6/percobaan3/Mahasiswa.java
@@ -0,0 +1,29 @@
+package pertemuan6.percobaan3;
+
+public class Mahasiswa {
+    private String nama;
+    private int thnMasuk;
+    private int umur;
+    private double ipk;
+
+    public Mahasiswa(String nama, int thnMasuk, int umur, double ipk) {
+        this.nama = nama;
+        this.thnMasuk = thnMasuk;
+        this.umur = umur;
+        this.ipk = ipk;
+    }
+
+    void tampil () {
+        String tampil = """
+                Nama        = %s
+                Tahun Masuk = %d
+                Umur        = %d
+                IPK         = %.2f
+                """.formatted(nama, thnMasuk, umur, ipk);
+        System.out.print(tampil);
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+}
