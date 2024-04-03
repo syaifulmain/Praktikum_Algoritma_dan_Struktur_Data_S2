54e8c4be0bbd5c8a1a9efba45e2fc2ae58356ad9
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-04-04 03:27:34 +0700

    konf

--- src/main/java/pertemuan7/latihanpraktikum/PencarianBuku15.java
+++ src/main/java/pertemuan7/latihanpraktikum/PencarianBuku15.java
@@ -39,16 +39,11 @@ public class PencarianBuku15 {
     }
 
     public void tampilData(String x, int pos, SortBy sortBy) {
-        if (pos != -1) {
-            int[] dupe = duplicateCheck(x, sortBy);
-            if (dupe.length > 1) {
-                System.out.println("Data \"" + x + "\" ditemukan lebih dari satu");
-                for (int i : dupe) {
-                    listBuku[i].tampilDataBuku();
-                }
-            } else {
-                listBuku[pos].tampilDataBuku();
-            }
+        Buku[] listBuku = sortBy(sortBy);
+        if (pos >= 0) {
+            listBuku[pos].tampilDataBuku();
+        } else if (pos == -2) {
+            System.out.println("Data \"" + x + "\" ditemukan lebih dari satu");
         } else {
             System.out.println("Data \"" + x + "\" tidak ditemukan");
         }
@@ -56,23 +51,41 @@ public class PencarianBuku15 {
 
     private Buku[] sortBy(SortBy sortBy) {
         Buku[] listBukuCopy = Arrays.copyOf(this.listBuku, this.listBuku.length);
-        Arrays.sort(listBuku, (o1, o2) -> o1.compareTo(o2, sortBy));
+        Arrays.sort(listBukuCopy, (o1, o2) -> o1.compareTo(o2, sortBy));
         return listBukuCopy;
     }
 
     private int sequentialSearch(Buku[] listBuku, String cari, SortBy sortBy) {
+        int[] temp = new int[0];
         for (int i = 0; i < listBuku.length; i++) {
             if (listBuku[i].equals(cari, sortBy)) {
-                return i;
+                temp = Arrays.copyOf(temp, temp.length + 1);
+                temp[temp.length - 1] = i;
+            }
+            if (temp.length > 1) {
+                return -2;
+            }
+            if (temp.length == 1 && !listBuku[i].equals(cari, sortBy)) {
+                return temp[0];
             }
         }
-        return -1;
+        return temp[0];
     }
 
     private int BinarySearch(Buku[] listBuku, String cari, int left, int right, SortBy sortBy) {
         if (right >= left) {
-            int mid = left + (right - left) / 2;
+            int mid = (right + left) / 2;
             if (listBuku[mid].equals(cari, sortBy)) {
+                if (mid >= 0 && mid < listBuku.length - 1) {
+                    if (listBuku[mid].equals(listBuku[mid + 1], sortBy)) {
+                        return -2;
+                    }
+                }
+                if (mid > 0 && mid <= listBuku.length - 1) {
+                    if (listBuku[mid].equals(listBuku[mid - 1], sortBy)) {
+                        return -2;
+                    }
+                }
                 return mid;
             } else if (listBuku[mid].compareTo(cari, sortBy) > 0) {
                 return BinarySearch(listBuku, cari, left, mid - 1, sortBy);
@@ -81,19 +94,5 @@ public class PencarianBuku15 {
             }
         }
         return -1;
-    }
-
-    private int[] duplicateCheck(String check, SortBy sortBy) {
-        int[] dupe = new int[0];
-        for (int i = 0; i < listBuku.length; i++) {
-            if (listBuku[i].equals(check, sortBy)) {
-                dupe = Arrays.copyOf(dupe, dupe.length + 1);
-                dupe[dupe.length - 1] = i;
-            }
-            if (dupe.length > 1 && !listBuku[i].equals(check, sortBy)) {
-                break;
-            }
-        }
-        return dupe;
     }
 }
