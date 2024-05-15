c1012c89568ec5e6746e26786f84c8ac4d4d4938
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-05-15 12:10:46 +0700

    percobaan 2

---
+++ src/main/java/pertemuan12/percobaan2/DoubleLinkedListsMain.java
@@ -0,0 +1,29 @@
+package pertemuan12.percobaan2;
+
+public class DoubleLinkedListsMain {
+    public static void main(String[] args) throws Exception {
+        DoubleLinkedLists dll = new DoubleLinkedLists();
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==================================");
+
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==================================");
+
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==================================");
+
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==================================");
+    }
+}
