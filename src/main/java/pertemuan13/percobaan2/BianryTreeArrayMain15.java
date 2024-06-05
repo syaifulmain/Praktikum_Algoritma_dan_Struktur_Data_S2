7c82749f6230c2046cc8a7f4b64f3f7037fbf488
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-06-05 19:39:37 +0700

    percobaan 2

--- src/main/java/pertemuan13/percobaan2/BianryTreeArrayMain15.java
+++ src/main/java/pertemuan13/percobaan2/BianryTreeArrayMain15.java
@@ -1,2 +1,13 @@
-package pertemuan13.percobaan2;public class BianryTreeArrayMain15 {
+package pertemuan13.percobaan2;
+
+public class BianryTreeArrayMain15 {
+    public static void main(String[] args) {
+        BinaryTreeArray15 bta = new BinaryTreeArray15();
+        int[] data = {6, 4, 8, 3, 5, 7, 9, 0, 0, 0};
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+        System.out.print("\nInOrder Traversal : ");
+        bta.traverseInOrder(0);
+        System.out.println();
+    }
 }
--- src/main/java/pertemuan13/percobaan2/BinaryTreeArray15.java
+++ src/main/java/pertemuan13/percobaan2/BinaryTreeArray15.java
@@ -1,2 +1,23 @@
-package pertemuan13.percobaan2;public class BinaryTreeArray15 {
+package pertemuan13.percobaan2;
+
+public class BinaryTreeArray15 {
+    int[] data;
+    int idxLast;
+
+    public BinaryTreeArray15() {
+        data = new int[10];
+    }
+
+    void populateData(int[] data, int idxLast) {
+        this.data = data;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traverseInOrder(2 * idxStart + 1);
+            System.out.print(data[idxStart] + " ");
+            traverseInOrder(2 * idxStart + 2);
+        }
+    }
 }
