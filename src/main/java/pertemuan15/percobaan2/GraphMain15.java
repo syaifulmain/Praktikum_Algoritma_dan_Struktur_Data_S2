28877db7f4c91f7718a82a78a69be06bd4120e1b
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2024-06-05 20:27:31 +0700

    percobaan 2

---
+++ src/main/java/pertemuan15/percobaan2/GraphMain15.java
@@ -0,0 +1,17 @@
+package pertemuan15.percobaan2;
+
+public class GraphMain15 {
+    public static void main(String[] args) {
+        GraphMatriks15 gdg = new GraphMatriks15(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
+    }
+}
---
+++ src/main/java/pertemuan15/percobaan2/GraphMatriks15.java
@@ -0,0 +1,33 @@
+package pertemuan15.percobaan2;
+
+public class GraphMatriks15 {
+    int vertex;
+    int[][] matriks;
+
+    public GraphMatriks15(int vertex) {
+        this.vertex = vertex;
+        matriks = new int[vertex][vertex];
+    }
+
+    public void makeEdge(int asal, int tujuan, int jarak) {
+        matriks[asal][tujuan] = jarak;
+    }
+
+    public void removeEdge(int asal, int tujuan) {
+        matriks[asal][tujuan] = 0;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < vertex; i++) {
+            System.out.print("Gedung " + (char) ('A' + i) + " : ");
+            for (int j = 0; j < vertex; j++) {
+                if (matriks[i][j] != -1) {
+                    System.out.print("Gedung " + (char) ('A' + j) + " (" + matriks[i][j] + " m), ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+}
